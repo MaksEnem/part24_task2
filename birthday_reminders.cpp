67db2c6912a797cd280f8908f965c35c3b494a48
MaksEnem <110655471+MaksEnem@users.noreply.github.com>
2023-09-10 10:41:50 +1100

    finish

--- birthday_reminders.cpp
+++ birthday_reminders.cpp
@@ -50,7 +50,14 @@ void load_friends_birthsday(data& friends, std::tm& today) {
 
 	if (in_file.is_open()) {
 
-		
+		std::vector <std::string> vec_name(0);
+		std::vector <int> vec_day(0);
+		std::vector <int> vec_month(0);
+
+		int day_min = 32;
+		int month_min = 13;
+		bool is_birthsday = true;
+		bool is_examination = true;
 		while (!in_file.eof()) {
 
 			friends.name = "";
@@ -61,23 +68,68 @@ void load_friends_birthsday(data& friends, std::tm& today) {
 			if (friends.month == today.tm_mon && friends.day == today.tm_mday) {
 
 				std::cout << "Today is " << friends.name << "'s bithsday!!!";
+				is_birthsday = false;
+			}
+			else if (is_birthsday == true && friends.month == today.tm_mon && friends.day > today.tm_mday) {				
 
+				if (friends.day < day_min) {					
+
+					vec_name.push_back(friends.name);
+					vec_day.push_back(friends.day);
+					vec_month.push_back(friends.month);
+
+				}
+				else if (friends.day == day_min) {
+
+					vec_name.push_back(friends.name);
+					vec_day.push_back(friends.day);
+					vec_month.push_back(friends.month);
+
+				}
+
+				bool is_examination = false;
+				
+			}
+			else if (is_birthsday == true && is_examination == true && friends.month > today.tm_mon) {
+
+				if (friends.month < month_min) {
+
+					vec_name.push_back(friends.name);
+					vec_day.push_back(friends.day);
+					vec_month.push_back(friends.month);
+
+				}
+				else if (friends.month == month_min) {
+
+					if (friends.day < vec_day[0]) {
+
+						vec_name.push_back(friends.name);
+						vec_day.push_back(friends.day);
+						vec_month.push_back(friends.month);
+
+					}
+				}				
 			}
 
-
+			month_min = friends.month;
+			day_min = friends.day;
 
 			if (friends.name == "") {
 
 				break;
 
 			}
-
-			/*std::cout << friends.name<< ": " << friends.year << "/" << friends.month << "/" << friends.day << std::endl;*/
-
-			
-			
 		}
 		
+		if (vec_name.size() > 0) {
+
+			for (int i = 0; i < vec_name.size(); ++i) {
+
+				std::cout << "The next birthday is a " << vec_name[i] << " month: " << vec_month[i] << " ,day: " << vec_day[i] << std::endl;
+
+			}
+		}
+
 		in_file.close();
 
 	}
@@ -92,10 +144,8 @@ void load_friends_birthsday(data& friends, std::tm& today) {
 
 void experiment(std::tm& today){
 
-
 	std::cout << today.tm_mon << " " << today.tm_mday << std::endl;
 }
-
 
 int main() {	
 	
@@ -110,8 +160,5 @@ int main() {
 	++today.tm_mon;		
 	
 	load_friends_birthsday(friends, today);
-
-	
-
 	
 }
